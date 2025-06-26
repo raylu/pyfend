30cbf4c8588bff0620f5656504a279f622baff32
raylu <90059+raylu@users.noreply.github.com>
2025-06-25 22:24:41 -0700

    convert to declarative module

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,11 @@
-use pyo3::{
-	Bound,
-	prelude::{PyResult, pyfunction},
-	pymodule,
-	types::{PyModule, PyModuleMethods},
-	wrap_pyfunction,
-};
-
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-	Ok((a + b).to_string())
-}
+use pyo3::pymodule;
 
 #[pymodule]
-fn pyfend(m: &Bound<'_, PyModule>) -> PyResult<()> {
-	m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-	Ok(())
+mod pyfend {
+	use pyo3::prelude::{PyResult, pyfunction};
+
+	#[pyfunction]
+	fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+		Ok((a + b).to_string())
+	}
 }
