eae0b42ab7b275502c4c19e601f4e27fa95d16d7
raylu <90059+raylu@users.noreply.github.com>
2025-06-25 22:47:30 -0700

    FendError

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use pyo3::pymodule;
+use pyo3::{create_exception, pymodule};
 
 #[pymodule]
 mod pyfend {
@@ -9,7 +9,17 @@ mod pyfend {
 		let mut context = fend_core::Context::new();
 		match fend_core::evaluate(input, &mut context) {
 			Ok(output) => Ok(output.get_main_result().to_string()),
-			Err(e) => Err(pyo3::exceptions::PyException::new_err(e.to_string())),
+			Err(err_msg) => Err(FendError::new_err(err_msg)),
 		}
 	}
+
+	#[pymodule_export]
+	use super::FendError;
 }
+
+create_exception!(
+	pyfend,
+	FendError,
+	pyo3::exceptions::PyException,
+	"fend evaluation error"
+);
