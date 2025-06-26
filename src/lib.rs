c8a1a85507683ca0333c4cd5ab62861d74bdf08b
raylu <90059+raylu@users.noreply.github.com>
2025-06-25 23:20:01 -0700

    random

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,13 @@ use pyo3::{create_exception, pymodule};
 
 #[pymodule]
 mod pyfend {
+	use nanorand::Rng as _;
 	use pyo3::prelude::{PyResult, pyfunction};
 
 	#[pyfunction]
 	fn evaluate(input: &str) -> PyResult<String> {
 		let mut context = fend_core::Context::new();
+		context.set_random_u32_fn(|| nanorand::WyRand::new().generate::<u32>());
 		match fend_core::evaluate(input, &mut context) {
 			Ok(output) => Ok(output.get_main_result().to_string()),
 			Err(err_msg) => Err(FendError::new_err(err_msg)),
--- test.py
+++ test.py
@@ -6,6 +6,9 @@ class TestFend(unittest.TestCase):
 	def test_evaluate(self):
 		self.assertEqual(pyfend.evaluate('1+2'), '3')
 
+	def test_random(self):
+		pyfend.evaluate('roll 1d6')
+
 	def test_error(self):
 		with self.assertRaises(pyfend.FendError):
 			pyfend.evaluate('1+')
