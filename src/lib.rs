1f3b6970358c3ba07f18a074999fbc80aad5cf1b
raylu <90059+raylu@users.noreply.github.com>
2025-06-25 22:31:37 -0700

    pyfend.evaluate

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,11 @@ mod pyfend {
 	use pyo3::prelude::{PyResult, pyfunction};
 
 	#[pyfunction]
-	fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-		Ok((a + b).to_string())
+	fn evaluate(input: &str) -> PyResult<String> {
+		let mut context = fend_core::Context::new();
+		match fend_core::evaluate(input, &mut context) {
+			Ok(output) => Ok(output.get_main_result().to_string()),
+			Err(e) => Err(pyo3::exceptions::PyException::new_err(e.to_string())),
+		}
 	}
 }
--- test.py
+++ test.py
@@ -3,5 +3,5 @@ import unittest
 import pyfend
 
 class TestFend(unittest.TestCase):
-	def test_upper(self):
-		self.assertEqual(pyfend.sum_as_string(1, 2), '3')
+	def test_evaluate(self):
+		self.assertEqual(pyfend.evaluate('1+2'), '3')
