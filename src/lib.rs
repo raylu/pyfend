46c205e78677ad6abe19181f481be9eba99dfb7e
raylu <90059+raylu@users.noreply.github.com>
2025-06-26 21:53:18 -0700

    context

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,28 @@ use pyo3::{create_exception, pymodule};
 #[pymodule]
 mod pyfend {
 	use nanorand::Rng as _;
-	use pyo3::prelude::{PyResult, pyfunction};
+	use pyo3::{
+		prelude::{PyResult, pyfunction},
+		pyclass, pymethods,
+	};
 
 	#[pyfunction]
-	fn evaluate(input: &str) -> PyResult<String> {
-		let mut context = fend_core::Context::new();
-		context.set_random_u32_fn(|| nanorand::WyRand::new().generate::<u32>());
-		match fend_core::evaluate(input, &mut context) {
+	fn evaluate(input: &str, context: &mut Context) -> PyResult<String> {
+		let Context(fend_context) = context;
+		fend_context.set_random_u32_fn(|| nanorand::WyRand::new().generate::<u32>());
+		match fend_core::evaluate(input, fend_context) {
 			Ok(output) => Ok(output.get_main_result().to_string()),
 			Err(err_msg) => Err(FendError::new_err(err_msg)),
+		}
+	}
+
+	#[pyclass]
+	struct Context(fend_core::Context);
+	#[pymethods]
+	impl Context {
+		#[new]
+		fn new() -> Self {
+			Context(fend_core::Context::new())
 		}
 	}
 
--- test.py
+++ test.py
@@ -4,11 +4,16 @@ import pyfend
 
 class TestFend(unittest.TestCase):
 	def test_evaluate(self):
-		self.assertEqual(pyfend.evaluate('1+2'), '3')
+		self.assertEqual(pyfend.evaluate('1+2', pyfend.Context()), '3')
+
+	def test_variable(self):
+		context = pyfend.Context()
+		pyfend.evaluate('a = 1 + 2', context)
+		self.assertEqual(pyfend.evaluate('a + 3', context), '6')
 
 	def test_random(self):
-		pyfend.evaluate('roll 1d6')
+		pyfend.evaluate('roll 1d6', pyfend.Context())
 
 	def test_error(self):
 		with self.assertRaises(pyfend.FendError):
-			pyfend.evaluate('1+')
+			pyfend.evaluate('1+', pyfend.Context())
